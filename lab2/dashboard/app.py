407a1ca639b7e80ec3851bb8a65cb15924e574ca
Manuel Eiweck <manuel.eiweck@bkf.at>
2021-01-09 22:56:32 +0100

    add more barcharts

--- lab2/dashboard/app.py
+++ lab2/dashboard/app.py
@@ -32,12 +32,33 @@ topPDF = topDataset(50,pDF)
 # GRAPH 1
 barFig = barChartAllYears(pDF)
 
+barFig2 = barChartPerYear(pDF)
+
+top=50
+barFig3 = barChartPerYearTopN(pDF,top)
+
 app.layout = html.Div(children=[
     html.H1(children='Steam Game Data'),
+
+    html.H2(children='Total Playtime'),
 
     dcc.Graph(
         id='barChart-all',
         figure=barFig
+    ),
+
+    html.H2(children='Total Playtime per Release Year'),
+
+    dcc.Graph(
+        id='barChart-perYear',
+        figure=barFig2
+    ),
+
+    html.H2(children='Total Playtime per Release Year top 50 Games'),
+
+    dcc.Graph(
+        id='barChart-perYearAndName',
+        figure=barFig3
     )
 
 ])
--- lab2/dashboard/barchart.py
+++ lab2/dashboard/barchart.py
@@ -4,3 +4,13 @@ def barChartAllYears(pDF):
     df1 = pDF[['total_playtime','type']].groupby(['type']).sum().reset_index()
     fig = px.bar(df1, x='type', y='total_playtime')
     return fig
+
+def barChartPerYear(pDF):
+    df1 = pDF[['total_playtime','release_year','type']].groupby(['release_year','type']).sum().reset_index()
+    fig = px.bar(df1, x='release_year', y='total_playtime',color='type')
+    return fig
+
+def barChartPerYearTopN(pDF,n):
+    df1 = pDF[['total_playtime','release_year','name']].sort_values(by='total_playtime',ascending=False).head(n).groupby(['release_year','name']).sum().reset_index()
+    fig = px.bar(df1, x='release_year', y='total_playtime',color='name')
+    return fig
